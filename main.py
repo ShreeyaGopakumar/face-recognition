9c462bffef714386f112973fd86e431cd0f45fab
ShreeyaGopakumar <sgopakum@andrew.cmu.edu>
2024-07-05 10:45:15 -0400

    trial

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python3
+
 import cv2
+import sys
+
+print(sys.path)  # Print Python path for debugging
+print(cv2.__version__)  # Print OpenCV version for verification
 
 capture = cv2.VideoCapture(0)
 
@@ -9,7 +15,7 @@ while True:
     cv2.imshow("Capture Window", frame)  
 
     if (cv2.waitKey(1) == ord('s')):
-        cv2.imwrite(filename='capture.jpg',img=frame)
+        cv2.imwrite(filename='capture.jpg', img=frame)
         capture.release()
         new_image = cv2.imread('capture.jpg', cv2.IMREAD_GRAYSCALE)
         break
@@ -19,4 +25,5 @@ while True:
     elif cv2.getWindowProperty("Capture Window", cv2.WND_PROP_VISIBLE) < 1:
         capture.release()
         break
+
 cv2.destroyAllWindows()
